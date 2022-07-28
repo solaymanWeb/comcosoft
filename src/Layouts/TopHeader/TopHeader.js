af421f67b5973857044eca55598471e1a6be855c
solayman <solaymanweb27@gmail.com>
2022-07-28 01:20:13 -0700

    vs code editor testiong

---
+++ src/Layouts/TopHeader/TopHeader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TopHeader = () => {
+    return (
+        <div>
+               <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container> 
+        </div>
+    );
+};
+
+export default TopHeader;
